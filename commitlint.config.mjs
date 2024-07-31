831868085f9834d217c1029a48c907331f0b7051
davidho <dklho@hendrickscorp.com>
2024-07-31 23:25:22 +0800

    feat(setup): Add setup script
    - install dependencies
    - add .env
    - setup lint-staged

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,27 +1,29 @@
-export default {
-  extends: ["@commitlint/config-conventional"],
+const commitLintConfig = {
+  extends: ['@commitlint/config-conventional'],
   rules: {
-    "type-enum": [
+    'type-enum': [
       2,
-      "always",
+      'always',
       [
-        "build",
-        "chore",
-        "ci",
-        "docs",
-        "feat",
-        "fix",
-        "perf",
-        "refactor",
-        "revert",
-        "style",
-        "test"
-      ]
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+      ],
     ],
-    "header-max-length": [2, "always", 100],
-    "scope-enum": [2, "always"],
-    "body-max-length": [2, "always", 300],
-    "footer-max-line-length": [2, "always", 100],
-    "subject-case": [2, "always", "sentence-case"]
-  }
-};
+    'header-max-length': [2, 'always', 100],
+    'scope-enum': [2, 'always'],
+    'body-max-length': [2, 'always', 300],
+    'footer-max-line-length': [2, 'always', 100],
+    'subject-case': [2, 'always', 'sentence-case'],
+  },
+}
+
+export default commitLintConfig
